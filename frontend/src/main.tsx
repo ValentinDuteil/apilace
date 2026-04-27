36924afd7f61f622cdfd0f368897ac7005eeb489
ValentinDuteil <contactvalentinduteil@gmail.com>
2026-04-27 18:00:58 +0200

    feat(frontend): add protected and admin route guards, update main providers

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
 import { system } from './theme/index'
+import { CartProvider } from './contexts/CartContext.tsx'
+import { AuthProvider } from './contexts/AuthContext.tsx'
 import App from './App.tsx'
 import './index.css'
 
@@ -10,7 +12,11 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <ChakraProvider value={system}>
-        <App />
+        <AuthProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </AuthProvider>
       </ChakraProvider>
     </BrowserRouter>
   </StrictMode>,
